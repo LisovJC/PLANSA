8370dcf4dea143706c77fa57781c8596ee205987
Ваня Лисов <Foksver-Burakov@rambler.ru>
2022-05-03 15:21:46 +0300

    fix

--- ViewModel/Windows/ReviewViewModel.cs
+++ ViewModel/Windows/ReviewViewModel.cs
@@ -1,18 +1,28 @@
 ﻿using PLANSA.Services;
+using PLANSA.View.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace PLANSA.ViewModel.Windows
 {
     internal class ReviewViewModel : Observer
     {
         public RelayCommand CloseWindow { get; set; }
+
+        private Page _choicePage = new MainPage();
+        public Page ChoicePage
+        {
+            get => _choicePage;
+            set => Set(ref _choicePage, value);
+        }
         public ReviewViewModel()
         {
+            ChoicePage = new MainPage();
             CloseWindow = new RelayCommand(o =>
             {
                 foreach (Window window in Application.Current.Windows)
